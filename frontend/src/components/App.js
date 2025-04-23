8bca581609ad1c093be49d76b2e614e2fdc5f425
Tyler-Vuu <tvuu@uw.edu>
2025-04-22 22:43:18 -0700

    added landing page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import ChatBot from './Chatbot';
 import GoalSetting from './GoalSetting';
 import GoalOverview from './GoalOverview';
+import Landing from './Landing';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<h1 style={{ padding: '2rem' }}>Welcome to the Dashboard</h1>} />
+        <Route path="/" element={<Navigate to="/Landing" />} />
+        <Route path="/Landing" element={<Landing />} />
         <Route path="/ai-mentor" element={<ChatBot />} />
         <Route path="/goal-tracking" element={<GoalSetting />} />
         <Route path="/goal-overview" element={<GoalOverview />} />
---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,49 @@
+import React from "react";
+import '../index.css'; 
+
+function Landing() {
+  return (
+    <div className="container">
+      <div className="dashboard">
+        <main className="main-content">
+          <div className="content-wrapper">
+            <h1 className="title">
+              We Analyze. You Succeed. <br />
+              Let's Get Started!
+            </h1>
+            <p className="subtitle">
+              Your journey to smarter decisions starts here. Complete the quiz
+              and access your customized dashboard.
+            </p>
+            <div className="card-container">
+              <div className="card">
+                <h2 className="card-title">
+                  AI Mentor
+                </h2>
+                <p className="card-text">
+                  The founder space is complex. Get advice and set goals with an AI mentor here!
+                </p>
+                <button className="card-button">
+                  <span>AI Mentor</span>
+                </button>
+              </div>
+              <div className="card">
+                <h2 className="card-title">
+                  Goal Tracker
+                </h2>
+                <p className="card-text">
+                  Discover how you can improve your traits! Track milestones and get personalized insights to grow faster!
+                </p>
+                <button className="card-button">
+                  <span>Goal Tracker</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
