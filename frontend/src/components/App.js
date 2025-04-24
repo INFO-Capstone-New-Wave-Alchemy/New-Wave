9b7946a3f381bc935cb26ba068e6b6c5370afcc7
Tyler-Vuu <tvuu@uw.edu>
2025-04-23 17:01:43 -0700

    temporary contact page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,6 +6,7 @@ import ChatBot from './Chatbot';
 import GoalSetting from './GoalSetting';
 import GoalOverview from './GoalOverview';
 import Landing from './Landing';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/ai-mentor" element={<ChatBot />} />
         <Route path="/goal-tracking" element={<GoalSetting />} />
         <Route path="/goal-overview" element={<GoalOverview />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import '../index.css';
+
+function Contact() {
+  return (
+    <div className="container">
+      <div className="dashboard">
+        <main className="main-content">
+          <div className="content-wrapper">
+            <h1 className="title">
+              Have a Question?
+            </h1>
+            <p className="subtitle">
+              Contact us:
+            </p>
+            <p className="subtitle">
+              <h3>Email: www.thenewwaveventure.io</h3>
+            </p>
+            <p className="subtitle">
+                <h3>Phone: +1 (719) 565-9196</h3>
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
